3ebbe1aac613d75a04328b061e2c71ec6729fd15
Naimul Haque <naimul.on@gmail.com>
2020-12-23 19:51:59 +0600

    Added DB Connection

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
-echo "Hello world";
+require_once("config.php");
+$result = mysqli_query($con, "select * from products");
+
+if (mysqli_num_rows($result) > 0) {
+  while( $data = mysqli_fetch_array($result) ) {
+    
+  }
+}
+
 
 ?>
